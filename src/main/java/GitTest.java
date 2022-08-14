7c1b89b237c6da12f799489004363328b6c26c96
yefan <2083923479@qq.com>
2022-08-14 14:44:43 +0800

    hot-fix second

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix");
     }
 }
